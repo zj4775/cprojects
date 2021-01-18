c74b91ba7960492b058296c8698e9f83e2404383
zj4775 <0000zhongjie0000@163.com>
2021-01-18 08:54:11 +0800

    bb

--- main.cpp
+++ main.cpp
@@ -5,7 +5,61 @@
 #include "iomanip"
 using namespace std;
 
-typedef struct ;
+typedef struct date{
+    int year;
+    int month;
+    int day;
+};
+
+bool is_rn(int year){
+    if (year%400==0||(year%100!=0&&year%4==0))
+        return true;
+    return false;
+}
+
+void date_diff(){
+    date d1;
+    date d2;
+    int diff=0;
+    int month[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+    cout<<"请输入第一个日期"<<endl;
+    cin>>d1.year>>d1.month>>d1.day;
+    cout<<"请输入第二个日期"<<endl;
+    cin>>d2.year>>d2.month>>d2.day;
+    cout<<"第一个日期是："<<d1.year<<"-"<<d1.month<<"-"<<d1.day;
+    cout<<"第二个日期是："<<d2.year<<"-"<<d2.month<<"-"<<d2.day;
+    for (int i = d1.year; i <d2.year ; ++i) {
+        diff+=365;
+        if(is_rn(i)){
+            diff+=1;
+        }
+    }
+    int d1days=0;
+    for (int j = 1; j <=d1.month -1; ++j) {
+        d1days+=month[j-1];
+    }
+    if(d1.month>2&&is_rn(d1.year)){
+        d1days++;
+    }
+    d1days+=d1.day;
+
+    int d2days=0;
+    for (int j = 1; j <=d2.month -1; ++j) {
+        d2days+=month[j-1];
+    }
+    if(d2.month>2&&is_rn(d2.year)){
+        d2days++;
+    }
+    d2days+=d2.day;
+
+    int diffdays=diff+d2days-d1days;
+    cout<<"两个日期相差的天数是"<<diffdays;
+
+
+
+
+
+}
 
 void xiebian(){
     int a;
@@ -21,6 +75,7 @@ void xiebian(){
 
 
 int main(){
+    date_diff();
 
     return 0;
 }
