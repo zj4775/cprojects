b82ea97a1d39f46779400a2c082a7efdbb01ed29
jie <jerry.zhong@xinfei.cn>
2021-03-04 21:40:29 +0800

    company

---
+++ acm_渴求真实的天使.cpp
@@ -0,0 +1,94 @@
+//
+// Created by jie on 2021/3/4.
+//
+#include "iostream"
+#include <vector>
+#include <math.h>
+using namespace std;
+
+#define N 1000000000
+
+int gcd(int a,int b){
+    int max;
+    int min;
+    int k;
+    if (a>=b){
+        max=a;
+        min=b;
+    }else{
+        max=b;
+        min=a;
+    }
+    while (min){
+        k=max%min;
+        max=min;
+        min=k;
+    }
+    return max;
+}
+
+int muti_gcd(vector<int> v){
+    while (v.size()>1){
+        vector<int>::iterator first=v.begin();
+        vector<int>::iterator second=first++;
+        int x=gcd(*first,*second);
+        if (x==1){
+            return 1;
+        }else{
+            v.erase(first);
+            v.erase(second);
+            v.push_back(x);
+        }
+    }
+    return *v.begin();
+}
+
+
+int main(){
+    int n;
+    int k;
+    int x;
+    int gc;
+    vector<int> v;
+    cin>>n>>k;
+    cin>>x;
+    for (int j = 2; j <=sqrt(x) ; j++) {
+        if (x%j==0){
+            v.push_back(j);
+        }
+        if (j*j != x){
+            v.push_back(x/j);
+        }
+    }
+    if (v.empty()){
+        cout<<0;
+        return 0;
+    }
+    vector<int>::iterator it;
+    for (int i = 1; i <n ; i++) {
+        cin>>x;
+        for (it=v.begin();it!=v.end();) {
+            if (x%*it!=0){
+                it=v.erase(it);
+            }else{
+                it++;
+            }
+        }
+        if (v.empty()){
+            cout<<0;
+            return 0;
+        }
+    }
+
+    for (int i = k; i >1 ; i--) {
+        for (it=v.begin();it!=v.end();it++) {
+            if (i%*it==0){
+                cout<<i;
+                return 0;
+            }
+        }
+    }
+    cout<<0;
+
+    return 0;
+}
